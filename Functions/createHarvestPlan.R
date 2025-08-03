260d0aa1866926aa4527532859181b1450b60d2d
lizallyn <lizallyn@uw.edu>
2025-08-03 10:35:54 -0700

    got rid of "Before" option, don't think we used that for anything in the end. Might be a few other obsolete options here too

--- Functions/createHarvestPlan.R
+++ Functions/createHarvestPlan.R
@@ -7,7 +7,6 @@ createHarvestPlan <- function(scenario = c("None", "Trap", "Boat"), timing, empt
     harvest_plan <- harvest_plan
   } else {
     if(timing == "During"){harvest_days <- fishy_days}
-    if(timing == "Before"){harvest_days <- c((fishy_days[1]-11):(fishy_days[1]+9), sealion_arrival:(sealion_arrival+10))}
     if(timing == "Fishing"){harvest_days <- fishery_dates}
     if(timing == "Trap During"){harvest_days <- which(trap_days == 1)}
     if(timing == "Weekly"){harvest_days <- which(harvest_schedule == 1)}
@@ -16,14 +15,6 @@ createHarvestPlan <- function(scenario = c("None", "Trap", "Boat"), timing, empt
     harvest_plan[harvest_days] <- scenario
   }
     
-  #   if(scenario == "Boat") {
-  #   harvest
-  #   harvest_plan[harvest_days] <- scenario
-  # } else if(scenario == "Boat") {
-  #   harvest_plan[harvest_days] <- scenario
-  # } else {
-  #   return("Not a valid scenario option. Try Again!")
-  # }
   return(harvest_plan)
 }
 
