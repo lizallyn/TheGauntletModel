82c2cbf139dd28a5878e57b2ed3dcd6e75b03608
lizallyn <lizallyn@uw.edu>
2025-08-03 12:32:13 -0700

    cleaning out unused scripts and functions

--- RunTheModel/Choose_Harvest_Options.R
+++ RunTheModel/Choose_Harvest_Options.R
@@ -4,24 +4,24 @@
 # scenario <- "None"
 
 ## Removals on a weekly schedule, days/week
-scenario <- "Boat"
-timing = "Weekly"
-days_per_week <- 7
-harvest_schedule <- rep(c(rep(0, 7-days_per_week), rep(1, days_per_week)), n_days/7)
-fear <- "Fear"
-participation <- 1
-catchability <- 0.1
-overlap_pv <- 0.5
-
-## Removals on a schedule with set gap days between single removal days
 # scenario <- "Boat"
 # timing = "Weekly"
-# days_gap <- range_gap[rep]
-# harvest_schedule <- rep_len(c(rep(0, days_gap), 1), n_days)
+# days_per_week <- 7
+# harvest_schedule <- rep(c(rep(0, 7-days_per_week), rep(1, days_per_week)), n_days/7)
 # fear <- "Fear"
 # participation <- 1
 # catchability <- 0.1
 # overlap_pv <- 0.5
+
+## Removals on a schedule with set gap days between single removal days
+scenario <- "Boat"
+timing = "Weekly"
+days_gap <- 2
+harvest_schedule <- rep_len(c(rep(0, days_gap), 1), n_days)
+fear <- "Fear"
+participation <- 1
+catchability <- 0.1
+overlap_pv <- 0.5
 
 ## Removals during entire salmon presence
 # scenario <- "Boat"
@@ -39,3 +39,4 @@ overlap_pv <- 0.5
 # reduction factor = % reduction in total population
 reduction <- 0
 source("Functions/hauloutReduction.R")
+
--- RunTheModel/loadLoopFunctions.R
+++ RunTheModel/loadLoopFunctions.R
@@ -3,13 +3,10 @@
 source("Functions/salmonSpeciesUpdate.R")
 source("Functions/decideForagingDestination.R")
 source("Functions/socialInfo.R")
-# source("Functions/collusion.R")
 source("Functions/getHarvested.R")
 source("Functions/receptivityX.R")
 source("Functions/learnX.R")
 source("Functions/learnY_2.R")
-# source("Functions/type3FuncRes.R")
-# source("Functions/linearFuncRes.R")
 source("Functions/updateLearning_3.R")
 source("Functions/makePlots.R")
 
